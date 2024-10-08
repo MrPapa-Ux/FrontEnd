2db08f9548f7e28782909ab69b5373bf6068e249
MrPapa-Ux <tosans977@gmail.com>
2024-10-08 09:36:02 +0800

    anjay

--- pertemuan-16/index.js
+++ pertemuan-16/index.js
@@ -17,4 +17,41 @@ setTimeout(() => {
 }, 3000);
 console.log("Proses 4");
 
-//2. Concurrent
+// Concurrent
+setTimeout(() => {
+  console.log("Proses 1");
+  setTimeout(() => {
+    console.log("Proses 2");
+    setTimeout(() => {
+      console.log("Proses 3");
+      setTimeout(() => {
+        console.log("Proses 4");
+      }, 3000); // Menunggu 3 detik untuk Proses 4
+    }, 3000); // Menunggu 2 detik untuk Proses 3
+  }, 3000); // Menunggu 1 detik untuk Proses 2
+}, 3000); // Menunggu 0.5 detik untuk Proses 1
+
+//Promise
+//Buat Promise sederhana4
+//1. Then - Catch
+
+// Pakai Promises yang sudah ada
+//1. fetch
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+//Mofification with async awai
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1"
+    );
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+fetchData();
